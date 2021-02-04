a076cc81681a464c0e616a5cd6bb83f6dd4c2e2c
Morten <mtnielsen89@gmail.com>
2021-02-04 14:35:18 +0100

    Refactored folder structure to use namespaces

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,19 @@
+// Auto bind decorator
+namespace App {
+  export function AutoBind(
+    _target: any,
+    _methodName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      enumerable: false,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
