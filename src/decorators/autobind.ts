28f76acbe8d9cf8f77e59f9fafac76d247006853
Morten <mtnielsen89@gmail.com>
2021-02-04 15:06:38 +0100

    Refactored to using modules for import/export

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,19 +1,17 @@
 // Auto bind decorator
-namespace App {
-  export function AutoBind(
-    _target: any,
-    _methodName: string,
-    descriptor: PropertyDescriptor
-  ) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      enumerable: false,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjDescriptor;
-  }
+export function AutoBind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
